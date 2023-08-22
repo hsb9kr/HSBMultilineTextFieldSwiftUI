aefebbf20225d87a23c7f6cd07f21f42f5e3c1c5
Red <hsb9kr@gmail.com>
2023-08-22 16:25:11 +0900

    Initialize commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MultilineTextFieldSwiftUI",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/hsb9kr/HSBMultilineTextFieldSwiftUI", from: "0.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/MultilineTextFieldSwiftUI/MultilineTextFieldSwiftUI.swift
+++
@@ -1,6 +0,0 @@
-public struct MultilineTextFieldSwiftUI {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
